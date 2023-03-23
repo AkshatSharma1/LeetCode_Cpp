e4fb3e27d1e7cc5a0e716a2ae0b4f110da4e5038
Akshat Ajay <89241496+AkshatSharma1@users.noreply.github.com>
2023-03-23 20:28:32 +0530

    Time: 30 ms (88.12%), Space: 71.5 MB (56.39%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,35 +10,61 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         
-        ListNode* dummy = new ListNode(0);
-        ListNode* temp = dummy;
-        int carry = 0;
+//         ListNode* dummy = new ListNode(0);
+//         ListNode* temp = dummy;
+//         int carry = 0;
         
-        while(l1 || l2 || carry){
+//         while(l1 || l2 || carry){
             
-            int sum = 0;
+//             int sum = 0;
             
-            if(l1){
-                sum+=l1->val;
-                l1 = l1->next;
-            }
-            if(l2){
-                sum+=l2->val;
-                l2 = l2->next;
-            }
+//             if(l1){
+//                 sum+=l1->val;
+//                 l1 = l1->next;
+//             }
+//             if(l2){
+//                 sum+=l2->val;
+//                 l2 = l2->next;
+//             }
             
-            sum += carry;
+//             sum += carry;
             
-            //push node to dummy node;
-            carry = sum/10;
+//             //push node to dummy node;
+//             carry = sum/10;
             
-            ListNode* node = new ListNode(sum%10);
-            temp->next = node;
-            temp = temp->next;
+//             ListNode* node = new ListNode(sum%10);
+//             temp->next = node;
+//             temp = temp->next;
             
-        }
-        return dummy->next;
+//         }
+//         return dummy->next;
+//     }
+    
+    //using recursion
+    
+    ListNode* add(ListNode* l1, ListNode* l2, int carry){
+        if(l1==NULL && l2==NULL && carry==0) return NULL;
+        
+        int sum = carry;
+        
+        sum += l1?l1->val:0;
+        sum += l2?l2->val:0;
+        
+        //new node
+        ListNode* newNode = new ListNode(sum%10);
+        carry = sum/10;
+        
+        if(l1) l1 = l1->next;
+        if(l2) l2 = l2->next;
+        
+        newNode->next = add(l1, l2, carry);
+        
+        return newNode;
+    }
+    
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2){
+        return add(l1, l2, 0); //carry -> 0
     }
 };
