684dbb26b8cfdfe6d17473e76a53331a4605dfe6
Akshat Ajay <89241496+AkshatSharma1@users.noreply.github.com>
2023-03-23 14:06:53 +0530

    Time: 527 ms (56.44%), Space: 138 MB (34.66%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,58 +1,27 @@
 class Solution {
-    
-private:
-    vector<int> parent;
-    
-    void make_set(int v){
-        parent[v] = v;
-    }
-    
-    int find_set(int v){
-        if (v == parent[v])
-            return v;
-        return find_set(parent[v]);
-    }
-    
-    void union_sets(int a, int b) {
-        a = find_set(a);
-        b = find_set(b);
-        // if (a != b)
-        //     parent[b] = a;
-        
-        //here we need to check if first value is less or more than second
-        if(a<b){
-            parent[b] = a;
-        }
-        else{
-            parent[a] = b;
-        }
-    }
-    
 public:
-    
-    //Union find syntax
     int minScore(int n, vector<vector<int>>& roads) {
-        
-        parent.resize(n+1);
-        
-        //initialise the parent array with all cities
-        for(int i=1;i<=n;i++){
-            make_set(i);
+        int ans = INT_MAX;
+        vector<vector<pair<int, int>>> gr(n+1);
+        for(auto edge : roads){ 
+            gr[edge[0]].push_back({edge[1], edge[2]}); // u-> {v, dis}
+            gr[edge[1]].push_back({edge[0], edge[2]}); // v-> {u, dis}
         }
-        
-        //now union logic implementation
-        for(auto &road: roads){
-            union_sets(road[0], road[1]);
-        }
-        
-        //now we need to start from '1' city and reach till 'n' city maintaining minDist
-        int city = find_set(1), minDist = INT_MAX;
-        
-        for(auto &road: roads){
-            if(find_set(road[0])==city){
-                minDist = min(minDist, road[2]);
+
+        vector<int> vis(n+1, 0);
+        queue<int> q;
+        q.push(1); vis[1] = 1;
+        while(!q.empty()){
+            auto node = q.front(); q.pop();
+            for(auto& [v, dis] : gr[node]){
+                ans = min(ans, dis);
+                if(vis[v]==0){
+                    vis[v] = 1;
+                    q.push(v);
+                }
             }
         }
-        return minDist;
+
+        return ans;
     }
 };
