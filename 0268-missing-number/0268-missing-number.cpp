cd4d63aa8c6aac768e9ddda75f0d88b24d331070
Akshat Ajay <89241496+AkshatSharma1@users.noreply.github.com>
2023-03-24 01:49:39 +0530

    Time: 29 ms (24.62%), Space: 17.8 MB (99.97%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+    
+    //Binary search
+//     int missingNumber(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
         
-        int low = 0, high = nums.size();
+//         int low = 0, high = nums.size();
         
-        while(low<high){
-            int mid = (low + high)/2;
+//         while(low<high){
+//             int mid = (low + high)/2;
             
-            if(nums[mid]>mid) high = mid;
-            else low = mid+1;
+//             if(nums[mid]>mid) high = mid;
+//             else low = mid+1;
+//         }
+//         return low;
+//     }
+    
+    //O(n) and O(1)
+    int missingNumber(vector<int> &nums){
+        int n = nums.size();
+        int totalSum = nums.size()*(nums.size() + 1)/2;
+        int sum = 0;
+        cout<<totalSum<<endl;
+        
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
         }
-        return low;
+        
+        return totalSum - sum;
     }
 };
