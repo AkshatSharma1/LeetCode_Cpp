d8d284fdb8ff3b044e9d493dc3c0394f051221e5
Akshat Ajay <89241496+AkshatSharma1@users.noreply.github.com>
2023-03-29 00:58:44 +0530

    Time: 9 ms (22.34%), Space: 9.8 MB (30.82%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -4,7 +4,7 @@ public:
     int minCost(int indx, vector<int> &days, vector<int> &costs, vector<int> &dp){
         
         int n = days.size();
-        int i;
+        // int i = indx;
         
         //BASE CASE:
         if(indx >= n) return 0;
@@ -15,12 +15,17 @@ public:
         int dayPass = costs[0] + minCost(indx+1, days, costs, dp);
         
         //7-day pass: skip 7 days if a 7-day pass is bought
-        for(i = indx; i < n && days[i]<days[indx] + 7; i++);
+        // for(i = indx; i < n && days[i]<days[indx] + 7; i++);
+        int i = indx;
+        while(i<n && days[i]<days[indx]+7){i++;}
         int weekPass = costs[1] + minCost(i, days, costs, dp);
         
         
         //30-Day pass
-        for(i = indx; i<n && days[i]<days[indx]+30; i++);
+        i = indx;
+        
+        // for(i = indx; i<n && days[i]<days[indx]+30; i++);
+        while(i<n && days[i]<days[indx]+30){i++;}
         int monthPass = costs[2] + minCost(i, days, costs, dp);
         
         dp[indx] = min({dayPass, weekPass, monthPass});
