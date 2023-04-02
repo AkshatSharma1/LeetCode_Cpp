cbca46b2061240be8138748417f2aa9d17f8e962
Akshat Ajay <89241496+AkshatSharma1@users.noreply.github.com>
2023-04-02 12:45:27 +0530

    Time: 257 ms (54.97%), Space: 97.3 MB (67.55%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -8,17 +8,24 @@ public:
         int n = spells.size();
         int m = potions.size();
         sort(potions.begin(), potions.end());
+        
+        //no need do inbuilt changes
         // vector<int> pairs(n, 0);
         
         for(int i=0;i<n;i++){
             
             int count = 0;
+            long long checkingCondition = ceil(success*1.0/spells[i]);
             
-            int leastStrengthReq = lower_bound(potions.begin(), potions.end(), ceil(success*1.0/spells[i])) - potions.begin();
+            if(checkingCondition > potions[m-1]) spells[i] = 0;
+            
+            int leastStrengthReq = lower_bound(potions.begin(), potions.end(), checkingCondition) - potions.begin();
             
             count = m - leastStrengthReq;
+            
             // cout<<count<<" ";
             // pairs[i] = count;
+            
             spells[i] = count;
             
         }
