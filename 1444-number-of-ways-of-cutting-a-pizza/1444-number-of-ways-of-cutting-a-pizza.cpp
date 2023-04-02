bc9c476449492c058c50a76665532a19609a0947
Akshat Ajay <89241496+AkshatSharma1@users.noreply.github.com>
2023-04-03 00:11:18 +0530

    Time: 46 ms (20.32%), Space: 8.4 MB (18.39%) - LeetHub

--- 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.cpp
+++ 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.cpp
@@ -1,29 +1,90 @@
 class Solution {
 public:
-    int ways(vector<string>& pizza, int k) {
-        int m = pizza.size(), n = pizza[0].size();
-        vector<vector<vector<int>>> dp(vector(k, vector(m, vector(n, -1))));
-        vector<vector<int>> preSum(vector(m+1, vector(n+1, 0)));
-
-        for (int r = m - 1; r >= 0; r--)
-            for (int c = n - 1; c >= 0; c--)
-                preSum[r][c] = preSum[r][c+1] + preSum[r+1][c] - preSum[r+1][c+1] + (pizza[r][c] == 'A');
+    const int MOD = 1e9+7;
+    //Recursive way
+    int cutWays(int row, int col, int n, int m, int k, vector<vector<int>> &apples, vector<vector<vector<int>>> &dp){
         
-        return dfs(m, n, k-1, 0, 0, dp, preSum);
-    }
-    int dfs(int m, int n, int k, int r, int c, vector<vector<vector<int>>>& dp, vector<vector<int>>& preSum) {
-        if (preSum[r][c] == 0) return 0; 
-        if (k == 0) return 1; 
-        if (dp[k][r][c] != -1) return dp[k][r][c];
+        //Base case: if k is 1 means one slice left. Check if it has enough apples
+        if(k==1 && apples[row][col]>=1){
+            dp[row][col][k] = 1;
+            // return 1;
+        }
+        
+        //Base case 2: no apples left
+        if(apples[row][col]<k) dp[row][col][k] = 0;
+        
+        if(dp[row][col][k]!=-1) return dp[row][col][k];
+        
+        //Making cuts
         int ans = 0;
-
-        for (int nr = r + 1; nr < m; nr++) 
-            if (preSum[r][c] - preSum[nr][c] > 0)
-                ans = (ans + dfs(m, n, k - 1, nr, c, dp, preSum)) % 1000000007;
-        for (int nc = c + 1; nc < n; nc++) 
-            if (preSum[r][c] - preSum[r][nc] > 0)
-                ans = (ans + dfs(m, n, k - 1, r, nc, dp, preSum)) % 1000000007;
-
-        return dp[k][r][c] = ans;
+        
+        //Horizontal cuts
+        for(int i=row+1;i<n;i++){
+            
+            //now check if a cut can be made here
+            int cuttingPosition = apples[i][col];
+            if(apples[row][col] - cuttingPosition >0) ans = (ans + cutWays(i, col, n,m, k-1, apples,dp))%MOD;            
+        }
+        
+        //Vertical cuts
+        for(int i=col+1;i<m;i++){
+            
+            int cuttingPosition = apples[row][i];
+            if(apples[row][col] - cuttingPosition > 0) ans = (ans + cutWays(row, i, n, m, k-1, apples, dp))%MOD;
+        }
+        
+        return dp[row][col][k] = ans;
+        // return ans;
+    }
+    
+    
+    int ways(vector<string>& pizza, int k) {
+        int n = pizza.size();
+        int m = pizza[0].size();
+        
+        vector<vector<int>> apples(n,vector<int>(m, 0));
+        
+        //Fill apples array
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                //if last box, val is 0 or 1 depending upon availability
+                // if((i==n-1 && j==m-1) && pizza[i][j]=='A') apples[i][j] = 1;
+                
+                if (pizza[i][j] == 'A') {
+                    // if there is an apple at this location, mark it as 1
+                    apples[i][j] = 1;
+                }
+                
+                if(i==n-1 && j==m-1){
+                    // apples[i][j] = (pizza[i][j]=='A')?1:0;
+                    continue;
+                }
+                
+                else if(i==n-1){
+                    // apples[i][j] += (apples[i][j+1] + (pizza[i][j]=='A')?1:0);
+                    apples[i][j] += apples[i][j+1];
+                }
+                else if(j==m-1){
+                    // apples[i][j] += (apples[i+1][j] + (pizza[i][j]=='A')?1:0);
+                    apples[i][j]+=apples[i+1][j];
+                }
+                else{
+                    // apples[i][j] += (apples[i+1][j] + apples[i][j+1] - apples[i+1][j+1] + (pizza[i][j]=='A')?1:0);
+                    apples[i][j]+=apples[i][j+1] + apples[i+1][j] - apples[i+1][j+1];
+                }
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                cout<<apples[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        
+        //now when apples array is filled, do recursion task
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(k+1,-1)));
+        
+        return cutWays(0,0,n,m,k,apples, dp);
     }
 };
