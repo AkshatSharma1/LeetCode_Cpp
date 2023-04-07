0e1ccc78135cb68ddde1d557bccbc9dd5f5ede40
Akshat Ajay <89241496+AkshatSharma1@users.noreply.github.com>
2023-04-08 02:08:19 +0530

    Time: 84 ms (39.43%), Space: 24.8 MB (98.55%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    
+    //BFS
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<int,int>> q;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || j==0 || i==n-1 || j==m-1){
+                    if(grid[i][j]==1){
+                        q.push({i,j});
+                        grid[i][j] = 2;
+                    }
+                }
+            }
+        }
+        
+        int drow[] = {-1,1,0,0};
+        int dcol[] = {0,0,1,-1};
+        
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            
+            for(int i=0;i<4;i++){
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+                
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1){
+                    q.push({nrow, ncol});
+                    grid[nrow][ncol] = 2;
+                }
+            }
+            
+        }
+        
+        int count = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1) count++;
+            }
+        }
+        
+        return count;
+    }
+};
